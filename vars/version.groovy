16e60570ac716ff3963dca0d6bb6e617fcd3afff
Rodrigo <=>
2020-01-22 15:29:23 -0300

    ddsddsd

--- vars/version.groovy
+++ vars/version.groovy
@@ -3,8 +3,10 @@
 def call(String dir = 'human') {
   echo "${dir}"
   cat image_version.txt
-  cd ${dir} && git add .
-  cd ${dir} && git commit -m "version:$(cat image_version.txt)"
-  cd ${dir} && git remote add template https://rodrigo.hernandez:19+Enero+1998@gitlab01.oaas.hg.com.uy/rodrigo.hernandez/ntdoctemplate.git
-  cd ${dir} && git push template master
+  cd ${dir}
+  echo $(($(cat image_version.txt) + 1)) > image_version.txt
+  git add .
+  git commit -m "version:$(cat image_version.txt)"
+  git remote add template https://rodrigo.hernandez:19+Enero+1998@gitlab01.oaas.hg.com.uy/rodrigo.hernandez/ntdoctemplate.git
+  git push template master
 }

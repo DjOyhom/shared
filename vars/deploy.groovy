17ee0443acd15438620168b6b678bf642a4e457a
Rodrigo <=>
2020-02-04 18:27:42 -0300

    sd

--- vars/deploy.groovy
+++ vars/deploy.groovy
@@ -1,7 +1,6 @@
 #!/usr/bin/env groovy
 
 def call(String dir = 'human') {
-  sh 'cd /tmp/hugo && echo \'{"apiVersion": "apps.openshift.io/v1","kind": "DeploymentConfig","metadata": {"name": "hugitoweb","namespace": "hugo"},"spec": {"triggers": [{"imageChangeParams": {"automatic": "true","containerNames":["hugitoweb"],"from": {"kind": "ImageStreamTag","name":"hugito:$(cat image_version.txt)","namespace": "hugo"}},"type": "ImageChange"},{"type": "ConfigChange"}]}\' > deploy.json'
   sh 'cat /tmp/hugo/deploy.json'
   sh 'cd /tmp/hugo && oc patch dc hugitoweb -f deploy.json -n hugo'
 }
